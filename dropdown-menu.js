2a7492f79e94e3244bbf004fd02014112a8dda66
Harshith Chiluveri <harshith.chiluveri@ucalgary.ca>
2026-09-16 14:49:16 -0600

    created a new file for the header and called the header to other files using javascript

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -1,3 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  fetch("header.html")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+
+      document.getElementById("header-container").innerHTML = data;
+
+        // Get the hamburger menu after the header has been added
+        const body = document.body;
+        const menuIcon = document.getElementById("menuIcon");
+        const slideinMenu = document.getElementById("slideinMenu");
+
+        menuIcon.addEventListener("click", function() {
+            event.stopPropagation();
+            slideinMenu.classList.toggle("open");
+        });
+
+        slideinMenu.addEventListener("click", function(event) {
+          event.stopPropagation();
+        })
+        
+        body.addEventListener("click", function() {
+          slideinMenu.classList.remove("open");
+        })
+    });
+
+});
+
 const body = document.body;
 const menuIcon = document.getElementById("menuIcon");
 const sildeinMenu = document.getElementById("slideinnMenu");
