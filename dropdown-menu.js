f0c0952ef5a79efd0e05a5a42e1b00fc58af6cf0
Harshith Chiluveri <harshith.chiluveri@ucalgary.ca>
2026-09-16 17:06:02 -0600

    added logo on the header and changed design of the contact page

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -21,28 +21,10 @@ document.addEventListener("DOMContentLoaded", function() {
         slideinMenu.addEventListener("click", function(event) {
           event.stopPropagation();
         })
-        
+
         body.addEventListener("click", function() {
           slideinMenu.classList.remove("open");
         })
     });
 
-});
-
-const body = document.body;
-const menuIcon = document.getElementById("menuIcon");
-const sildeinMenu = document.getElementById("slideinnMenu");
-
-menuIcon.addEventListener("click", function(event) {
-  event.stopPropagation();
-  slideinMenu.classList.toggle("open");
-})
-
-slideinMenu.addEventListener("click", function(event) {
-  event.stopPropagation();
-})
-
-body.addEventListener("click", function() {
-  slideinMenu.classList.remove("open");
-})
-
+  });
