87f1c8c7ee71a0119dd2ec3053d684f386d9ee48
Harshith Chiluveri <harshith.chiluveri@ucalgary.ca>
2026-09-20 04:55:35 -0600

    moved vanilla about code to react. Installed react router

---
+++ src/pages/About.jsx
@@ -0,0 +1,85 @@
+function About() {
+  return (
+    <section className="about">
+      <div className="about-page">
+        <p className="heading">
+          MEET THE TEAM
+        </p>
+        <div className="about-grid">
+          <div className="about-items">
+            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+            <div className="exec-info">
+                <p className="name">
+                <Strong>Alyssa</Strong>
+              </p>
+              <p className="position">
+                <Strong>President</Strong>
+              </p>
+            </div>
+          </div>
+            
+          
+          <div className="about-items">
+            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+            <div className="exec-info">
+              <p className="name">
+                <Strong>Andrew</Strong>
+              </p> 
+              <p className="position">
+                <Strong>Cosplay lead</Strong>
+              </p>
+              <p className="about-text">
+                Hi, my name is Andrew, otherwise known as Fatal. I'm currently a 2nd year political science major with goals to do continuing education into a BEd. Besides that I'm also a cosplayer and have been since May 2024! I love to sing, do funny skits, 3D print and wig style and a whole lot more! I'm also the guy who shows up to classNamees in cosplay just for the heck of it lmao.
+              </p>
+            </div>
+          </div>
+
+          <div className="about-items">
+            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+            <div className="exec-info">
+              <p className="name">
+                <Strong>Harsh</Strong>
+              </p>
+              <p className="position">
+                <Strong>Web Dev and Mascot Designer</Strong>
+              </p>
+            </div>
+          </div>
+
+          <div className="about-items">
+            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+            <div className="exec-info">
+              <p className="name">
+                <Strong>Jerry</Strong>
+              </p>
+              <p className="position">
+                <Strong>Sr. Exec</Strong>
+              </p>
+              <p className="about-text">
+                Heyo~ I'm Jerry/Akarui! I'm a 4th year linguistics student and I like to cosplay and do other things that relate to cosplay e.g. prop crafting, dancing, etc. I also play a lot of Japanese arcade rhythm games. よろしくね~!
+              </p>
+            </div>
+          </div>
+
+          <div className="about-items">
+            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+            <div className="exec-info">
+              <p className="name">
+                <Strong>KB</Strong>
+              </p>
+              <p className="position">
+                <Strong>Sr. Exec</Strong>
+              </p>
+              <p className="about-text">
+                Hello I'm KB and I'm second year MSc Chemical Engineering student. I like Hot topic browsing and making doujinshi or just fanfic/fanart, looking forward to see you all in our events
+              </p>
+            </div>
+          </div>
+          
+
+        </div>
+      </div>
+    </section>
+  );
+}
+
