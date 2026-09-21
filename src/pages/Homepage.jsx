10e533036abac212caf5c1690c23da0f23f0532d
Harshith Chiluveri <harshith.chiluveri@ucalgary.ca>
2026-09-21 15:37:30 -0600

    made some more design edits

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -18,26 +18,27 @@ function Homepage() {
             </p>
           </div>
 
-          <div>
+          <div className="home-buttons">
             <p className="heading">
               WEEKLY EVENTS EVERY FRIDAY
             </p>
             <p className="text">
               Join our weekly meetings every Friday 5pm to 9pm! We host several activities like movie nights, karaoke, anime trivia, and many more!
             </p>
-          </div>
-            
-            <Link to="events.html">
+
+            <Link to="/Events">
               <button className="upcoming-events">
                 Check Out Our Upcoming Events!
               </button>
             </Link>
+          </div>
+            
 
-          <div>
+          <div className="home-buttons">
             <p className="heading">
               GIVE US EVENT SUGGESTIONS
             </p>
-            <Link to="contact.html">
+            <Link to="/Contact">
               <button className="suggestions">
                 what kind of events would YOU be interested in!
 
@@ -69,7 +70,7 @@ function Homepage() {
         <p className="text">
           - Unrestricted Manga Library Access
         </p>
-        <p className="text">
+        <p className="more">
           - and more!
         </p>
       </section>
