bb2a9d1c61f4d02d5ab9efe6a8e2129494c823f1
Harshith Chiluveri <harshith.chiluveri@ucalgary.ca>
2026-09-22 06:38:10 -0600

    changing colors

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,83 +1,87 @@
 function About() {
   return (
-    <section className="about">
-      <div className="about-page">
-        <p className="heading">
-          MEET THE TEAM
-        </p>
-        <div className="about-grid">
-          <div className="about-items">
-            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
-            <div className="exec-info">
-                <p className="name">
-                <strong>Alyssa</strong>
-              </p>
-              <p className="position">
-                <strong>President</strong>
-              </p>
-            </div>
-          </div>
-            
-          
-          <div className="about-items">
-            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
-            <div className="exec-info">
+    <section className="about-page">
+      <p className="heading">
+        MEET THE TEAM
+      </p>
+      <div className="about-grid">
+        <div className="about-items">
+          <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+          <div className="exec-info">
               <p className="name">
-                <strong>Andrew</strong>
-              </p> 
-              <p className="position">
-                <strong>Cosplay lead</strong>
-              </p>
-              <p className="about-text">
-                Hi, my name is Andrew, otherwise known as Fatal. I'm currently a 2nd year political science major with goals to do continuing education into a BEd. Besides that I'm also a cosplayer and have been since May 2024! I love to sing, do funny skits, 3D print and wig style and a whole lot more! I'm also the guy who shows up to classNamees in cosplay just for the heck of it lmao.
-              </p>
-            </div>
+              <strong>Alyssa</strong>
+            </p>
+            <p className="position">
+              <strong>President</strong>
+            </p>
+            <p className="about-text">
+              ...
+            </p>
           </div>
-
-          <div className="about-items">
-            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
-            <div className="exec-info">
-              <p className="name">
-                <strong>Harsh</strong>
-              </p>
-              <p className="position">
-                <strong>Web Dev and Mascot Designer</strong>
-              </p>
-            </div>
-          </div>
-
-          <div className="about-items">
-            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
-            <div className="exec-info">
-              <p className="name">
-                <strong>Jerry</strong>
-              </p>
-              <p className="position">
-                <strong>Sr. Exec</strong>
-              </p>
-              <p className="about-text">
-                Heyo~ I'm Jerry/Akarui! I'm a 4th year linguistics student and I like to cosplay and do other things that relate to cosplay e.g. prop crafting, dancing, etc. I also play a lot of Japanese arcade rhythm games. よろしくね~!
-              </p>
-            </div>
-          </div>
-
-          <div className="about-items">
-            <img className="profile-pic" src="pictures/Luna-explaining.png"/>
-            <div className="exec-info">
-              <p className="name">
-                <strong>KB</strong>
-              </p>
-              <p className="position">
-                <strong>Sr. Exec</strong>
-              </p>
-              <p className="about-text">
-                Hello I'm KB and I'm second year MSc Chemical Engineering student. I like Hot topic browsing and making doujinshi or just fanfic/fanart, looking forward to see you all in our events
-              </p>
-            </div>
-          </div>
-          
-
         </div>
+          
+        
+        <div className="about-items">
+          <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+          <div className="exec-info">
+            <p className="name">
+              <strong>Andrew</strong>
+            </p> 
+            <p className="position">
+              <strong>Cosplay lead and Outreach</strong>
+            </p>
+            <p className="about-text">
+              Hi, my name is Andrew, otherwise known as Fatal. I'm currently a 2nd year political science major with goals to do continuing education into a BEd. Besides that I'm also a cosplayer and have been since May 2024! I love to sing, do funny skits, 3D print and wig style and a whole lot more! I'm also the guy who shows up to classes in cosplay just for the heck of it lmao.
+            </p>
+          </div>
+        </div>
+
+        <div className="about-items">
+          <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+          <div className="exec-info">
+            <p className="name">
+              <strong>Harsh</strong>
+            </p>
+            <p className="position">
+              <strong>Web Dev and Mascot Designer</strong>
+            </p>
+            <p className="about-text">
+              Hi, I'm Harsh and I'm a 3rd year computer science student. I like to draw and code (you probably could've guessed). Come say hi if you see me in the hallway :)
+            </p>
+          </div>
+        </div>
+
+        <div className="about-items">
+          <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+          <div className="exec-info">
+            <p className="name">
+              <strong>Jerry</strong>
+            </p>
+            <p className="position">
+              <strong>Sr. Exec</strong>
+            </p>
+            <p className="about-text">
+              Heyo~ I'm Jerry/Akarui! I'm a 4th year linguistics student and I like to cosplay and do other things that relate to cosplay e.g. prop crafting, dancing, etc. I also play a lot of Japanese arcade rhythm games. よろしくね~!
+            </p>
+          </div>
+        </div>
+
+        <div className="about-items">
+          <img className="profile-pic" src="pictures/Luna-explaining.png"/>
+          <div className="exec-info">
+            <p className="name">
+              <strong>KB</strong>
+            </p>
+            <p className="position">
+              <strong>Sr. Exec</strong>
+            </p>
+            <p className="about-text">
+              Hello I'm KB and I'm second year MSc Chemical Engineering student. I like Hot topic browsing and making doujinshi or just fanfic/fanart, looking forward to see you all in our events
+            </p>
+          </div>
+        </div>
+        
+
       </div>
     </section>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,75 +4,85 @@ function Homepage() {
   return (
     <main className="homepage-body">
       <img className="banner" id="banner" src="/pictures/Website-banner.png"/>
+      
       <section className="home">
-
-        <img className="mascot" src="public/pictures/Luna-Explaining.png"/>
         
+        <img className="home-logo" src="/pictures/Logo Version 2 (3).jpg"/>
         <div className="general-info">
           <div>
             <p className="heading">
               WHO ARE WE?
             </p>
-            <p className="text">
-              We are a student-led club at the University of Calgary that is dedicated to creating a community for everyone interested in anime/manga, cosplaying and Japanese culture.
+            <p>
+              We are a student-led, non-profit club at the University of Calgary that is dedicated to creating a community for everyone interested in anime/manga, cosplaying and Japanese culture. 
             </p>
           </div>
 
-          <div className="home-buttons">
+          <div className="time">
             <p className="heading">
               WEEKLY EVENTS EVERY FRIDAY
             </p>
-            <p className="text">
-              Join our weekly meetings every Friday 5pm to 9pm! We host several activities like movie nights, karaoke, anime trivia, and many more!
+            <p>
+              We host weekly meetings every Friday 5pm to 9pm! We host several activities like movie nights, karaoke, anime trivia, and many more!
             </p>
+          </div>
+
+          <div className="community">
+            <p className="heading">
+              COMMUNITY
+            </p>
+            <p>
+              Join the 100+ member community who all share the same interest! This is a great way to meet new people on campus...
+            </p>
+          </div>
+            
+
+          <div className="home-buttons">
 
             <Link to="/Events">
               <button className="upcoming-events">
                 Check Out Our Upcoming Events!
               </button>
             </Link>
-          </div>
-            
 
-          <div className="home-buttons">
-            <p className="heading">
-              GIVE US EVENT SUGGESTIONS
-            </p>
             <Link to="/Contact">
               <button className="suggestions">
                 what kind of events would YOU be interested in!
-
               </button>
             </Link>
+
           </div>
         </div>
   
       </section>
 
-
-      <section className="memberships">
-        <p className="heading">
-          MEMBERSHIP
-        </p>
-        <p className="text">
-          Become a member to get for just $5 to get exclusive offers and perks!
-        </p>  
-        
-        <p className="text">
-          Memeberships incudes:  
-        </p>
-        <p className="text">
-          - 50% off paid club events
-        </p>
-        <p className="text">
-          - Free snacks at events
-        </p>
-        <p className="text">
-          - Unrestricted Manga Library Access
-        </p>
-        <p className="more">
-          - and more!
-        </p>
+      
+      <section className="memberships-section">
+        <img className="mascot" src="public/pictures/Full-Luna-explaining.png"/>
+        <div className="memberships">          
+          <p className="heading">
+            MEMBERSHIP
+          </p>
+          <p>
+            Become a member to get for just $5 to get exclusive offers and perks!
+          </p>  
+          
+          <p>
+            <strong>Memeberships incudes: </strong> 
+          </p>
+          <p>
+            - 50% off paid club events
+          </p>
+          <p>
+            - Free snacks at events
+          </p>
+          <p>
+            - Unrestricted Manga Library Access
+          </p>
+          <p>
+            - and more!
+          </p>
+        </div>
       </section>
     </main>
   );
